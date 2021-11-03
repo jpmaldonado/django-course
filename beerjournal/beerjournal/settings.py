96d9bfffe697b23f8bcc87d3133397a189d5276f
Pablo <pablo@maldonado.cz>
2021-11-03 13:26:35 +0100

    static assets

--- beerjournal/beerjournal/settings.py
+++ beerjournal/beerjournal/settings.py
@@ -59,10 +59,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'beerjournal.urls'
 
+import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,6 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Files for local development
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+
+# Directory on the production server to serve static files from
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
